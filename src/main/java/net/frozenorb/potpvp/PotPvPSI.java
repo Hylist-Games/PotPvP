9fa6fb6f75c6cb116f0a088f13d7bd3e21bbd27c
Colin McDonald <cmcdonald.main@gmail.com>
2016-10-08 21:19:53 -0400

    Connect to Mongo and Redis upon startup

--- src/main/java/net/frozenorb/potpvp/PotPvPSI.java
+++ src/main/java/net/frozenorb/potpvp/PotPvPSI.java
@@ -1,22 +1,61 @@
 package net.frozenorb.potpvp;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 import lombok.Getter;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 public final class PotPvPSI extends JavaPlugin {
 
     @Getter private static PotPvPSI instance;
 
+    private MongoClient mongoClient;
+    @Getter private MongoDatabase mongoDatabase;
+
+    @Getter private JedisPool redisConnection;
+
     @Override
     public void onEnable() {
         instance = this;
         saveDefaultConfig();
+
+        setupMongo();
+        setupRedis();
     }
 
     @Override
     public void onDisable() {
         instance = null;
+
+        mongoClient.close();
+        redisConnection.close();
+    }
+
+    private void setupMongo() {
+        mongoClient = new MongoClient(
+            getConfig().getString("Mongo.Host"),
+            getConfig().getInt("Mongo.Port")
+        );
+
+        String databaseId = getConfig().getString("Mongo.Database");
+        mongoDatabase = mongoClient.getDatabase(databaseId);
+    }
+
+    private void setupRedis() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+
+        // Don't block any thread when pool is exhausted
+        jedisPoolConfig.setBlockWhenExhausted(false);
+
+        redisConnection = new JedisPool(
+            jedisPoolConfig,
+            getConfig().getString("Redis.Host"),
+            getConfig().getInt("Redis.Port")
+        );
     }
 
 }
