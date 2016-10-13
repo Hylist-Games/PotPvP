e643dce23751cc9329de9970d4ac878d28264b99
Colin McDonald <cmcdonald.main@gmail.com>
2016-10-12 22:16:50 -0400

    Add PotPvPLang file with common static strings

---
+++ src/main/java/net/frozenorb/potpvp/PotPvPLang.java
@@ -0,0 +1,13 @@
+package net.frozenorb.potpvp;
+
+import org.bukkit.ChatColor;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public final class PotPvPLang {
+
+    public static final String LEFT_ARROW_STRING = ChatColor.BLUE.toString() + ChatColor.BOLD + "» ";
+    public static final String RIGHT_ARROW_STRING = ChatColor.BLUE.toString() + ChatColor.BOLD + " «";
+
+}
