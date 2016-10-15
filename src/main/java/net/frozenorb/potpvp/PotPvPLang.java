8903f77188712bab6afeb8f08a91378452231e3d
Colin McDonald <cmcdonald.main@gmail.com>
2016-10-15 13:47:35 -0400

    Document PotPvPLang constants file

--- src/main/java/net/frozenorb/potpvp/PotPvPLang.java
+++ src/main/java/net/frozenorb/potpvp/PotPvPLang.java
@@ -4,10 +4,28 @@ import org.bukkit.ChatColor;
 
 import lombok.experimental.UtilityClass;
 
+/**
+ * Constants for messages/strings commonly shown to players
+ */
 @UtilityClass
 public final class PotPvPLang {
 
+    /**
+     * `&9&l» ` - Arrow used on the left side of item display names
+     * Named left arrow due to its usage on the left side of items, despite the fact
+     * the arrow is actually pointing to the right.
+     * @see net.frozenorb.potpvp.lobby.LobbyItems usage
+     * @see net.frozenorb.potpvp.queue.QueueItems usage
+     */
     public static final String LEFT_ARROW = ChatColor.BLUE.toString() + ChatColor.BOLD + "» ";
-    public static final String RIGHT_ARROW = ChatColor.BLUE.toString() + ChatColor.BOLD + " «";
+
+    /**
+     * ` &9&l«` - Arrow used on the right side of item display names
+     * Named right arrow due to its usage on the right side of items, despite the fact
+     * the arrow is actually pointing to the left.
+     * @see net.frozenorb.potpvp.lobby.LobbyItems usage
+     * @see net.frozenorb.potpvp.queue.QueueItems usage
+     */
+    public static final String RIGHT_ARROW = " " + ChatColor.BLUE.toString() + ChatColor.BOLD + "«";
 
 }
