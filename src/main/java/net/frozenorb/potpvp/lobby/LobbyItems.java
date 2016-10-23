d44ba14017339a8139d5ff29b852174fd89c4fb1
Colin McDonald <cmcdonald.main@gmail.com>
2016-10-23 16:54:03 -0400

    Extract duel related items into their own DuelItems class

--- src/main/java/net/frozenorb/potpvp/lobby/LobbyItems.java
+++ src/main/java/net/frozenorb/potpvp/lobby/LobbyItems.java
@@ -17,17 +17,11 @@ import static org.bukkit.ChatColor.YELLOW;
 @UtilityClass
 public final class LobbyItems {
 
-    public static final ItemStack OTHER_PARTIES_ITEM = new ItemStack(Material.SKULL_ITEM);
-    public static final ItemStack PENDING_INVITES_ITEM = new ItemStack(Material.WATCH);
-
     public static final ItemStack START_TEAM_SPLIT_ITEM = new ItemStack(Material.DIAMOND_SWORD);
 
     public static final ItemStack EVENTS_ITEM = new ItemStack(Material.EMERALD);
 
     static {
-        ItemUtils.setDisplayName(OTHER_PARTIES_ITEM, LEFT_ARROW + GREEN.toString() + BOLD + "Other Parties" + RIGHT_ARROW);
-        ItemUtils.setDisplayName(PENDING_INVITES_ITEM, LEFT_ARROW + YELLOW.toString() + BOLD + "Pending Invites" + RIGHT_ARROW);
-
         ItemUtils.setDisplayName(START_TEAM_SPLIT_ITEM, LEFT_ARROW + YELLOW.toString() + BOLD + "Start Team Split" + RIGHT_ARROW);
 
         ItemUtils.setDisplayName(EVENTS_ITEM, LEFT_ARROW + LIGHT_PURPLE.toString() + BOLD + "Events" + RIGHT_ARROW);
