6ca752ca18597a2269796b850a6d1141d6fed197
Colin McDonald <cmcdonald.main@gmail.com>
2016-10-23 15:11:10 -0400

    Add arena mangement commands

---
+++ src/main/java/net/frozenorb/potpvp/arena/command/ArenaListSchematicsCommand.java
@@ -0,0 +1,39 @@
+package net.frozenorb.potpvp.arena.command;
+
+import net.frozenorb.potpvp.PotPvPSI;
+import net.frozenorb.potpvp.arena.Arena;
+import net.frozenorb.potpvp.arena.ArenaHandler;
+import net.frozenorb.potpvp.arena.ArenaSchematic;
+import net.frozenorb.qlib.command.Command;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+public final class ArenaListSchematicsCommand {
+
+    @Command(names = { "arena listSchematics" }, permission = "op")
+    public static void arenaListSchematics(Player sender) {
+        ArenaHandler arenaHandler = PotPvPSI.getInstance().getArenaHandler();
+
+        sender.sendMessage(ChatColor.RED + "------ " + ChatColor.WHITE + "Schematics" + ChatColor.RED + " ------");
+
+        for (ArenaSchematic schematic : arenaHandler.getSchematics()) {
+            int totalCopies = 0;
+            int inUseCopies = 0;
+
+            for (Arena arena : arenaHandler.getArenas(schematic)) {
+                totalCopies++;
+
+                if (arena.isInUse()) {
+                    inUseCopies++;
+                }
+            }
+
+            String inUseStr = inUseCopies + "/" + totalCopies;
+            String rankedStr = schematic.isSupportsRanked() ? ChatColor.GREEN + "Ranked allowed" : ChatColor.RED + "Unranked only";
+
+            sender.sendMessage(schematic.getName() + ": " + ChatColor.GREEN + inUseStr + ChatColor.GRAY + " - " + rankedStr);
+        }
+    }
+
+}
