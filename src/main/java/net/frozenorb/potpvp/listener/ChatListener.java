2d25db2a59c3abae5747cc83f87a9de07e3f03b2
Colin McDonald <cmcdonald.main@gmail.com>
2016-10-23 15:30:56 -0400

    Add javadoc to ChatListener

--- src/main/java/net/frozenorb/potpvp/listener/ChatListener.java
+++ src/main/java/net/frozenorb/potpvp/listener/ChatListener.java
@@ -5,6 +5,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+/**
+ * Applies rank prefixes, if they exist.
+ * Copied from https://github.com/FrozenOrb/qHub/blob/master/src/main/java/net/frozenorb/qhub/listener/BasicListener.java#L103
+ */
 public final class ChatListener implements Listener {
 
     @EventHandler
