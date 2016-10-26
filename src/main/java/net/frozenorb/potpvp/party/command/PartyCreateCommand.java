42d0d23a9de21be813371da54198fc42cf4babda
Colin McDonald <cmcdonald.main@gmail.com>
2016-10-26 17:01:25 -0400

    Add /party create command. Closes #1

--- src/main/java/net/frozenorb/potpvp/party/command/PartyCreateCommand.java
+++ src/main/java/net/frozenorb/potpvp/party/command/PartyCreateCommand.java
@@ -1,5 +1,7 @@
 package net.frozenorb.potpvp.party.command;
 
+import net.frozenorb.potpvp.PotPvPSI;
+import net.frozenorb.potpvp.party.PartyHandler;
 import net.frozenorb.qlib.command.Command;
 
 import org.bukkit.ChatColor;
@@ -9,7 +11,15 @@ public final class PartyCreateCommand {
 
     @Command(names = {"party create", "p create", "t create", "team create", "f create"}, permission = "")
     public static void partyCreate(Player sender) {
-        sender.sendMessage(ChatColor.YELLOW + "To create a party, simply invite a player and a party will be created automatically.");
+        PartyHandler partyHandler = PotPvPSI.getInstance().getPartyHandler();
+
+        if (partyHandler.hasParty(sender)) {
+            sender.sendMessage(ChatColor.RED + "You are already in a party.");
+            return;
+        }
+
+        partyHandler.getOrCreateParty(sender);
+        sender.sendMessage(ChatColor.YELLOW + "Created a new party.");
     }
 
 }
