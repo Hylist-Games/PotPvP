b1c3c2798131d2bc44d9166f92ab09b36da6c608
Colin McDonald <cmcdonald.main@gmail.com>
2016-10-30 19:54:32 -0400

    Remove spectators from their party when leaving a match. Closes #24

--- src/main/java/net/frozenorb/potpvp/party/listener/PartyLeaveListener.java
+++ src/main/java/net/frozenorb/potpvp/party/listener/PartyLeaveListener.java
@@ -1,7 +1,9 @@
 package net.frozenorb.potpvp.party.listener;
 
 import net.frozenorb.potpvp.PotPvPSI;
+import net.frozenorb.potpvp.match.event.MatchSpectatorLeaveEvent;
 import net.frozenorb.potpvp.party.Party;
+import net.frozenorb.potpvp.party.PartyHandler;
 import net.frozenorb.potpvp.party.PartyInvite;
 
 import org.bukkit.entity.Player;
@@ -28,6 +30,20 @@ public final class PartyLeaveListener implements Listener {
             if (invite != null) {
                 party.revokeInvite(invite);
             }
+        }
+    }
+
+    // players who leave a match while in a party won't be able to
+    // do anything (most validation checks disallow players in a party)
+    // so as a convenience we remove them from their party.
+    // see @jhalt for any further explanation
+    @EventHandler
+    public void onMatchSpectatorLeave(MatchSpectatorLeaveEvent event) {
+        PartyHandler partyHandler = PotPvPSI.getInstance().getPartyHandler();
+        Party party = partyHandler.getParty(event.getSpectator());
+
+        if (party != null) {
+            party.leave(event.getSpectator());
         }
     }
 
