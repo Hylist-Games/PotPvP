2cf0d808514ea7bfe8b7acb03bece4e2d74993b2
Colin McDonald <cmcdonald.main@gmail.com>
2016-10-30 20:34:04 -0400

    Add 'raw' playing/spectating values to /pstatus for upcoming caching commit

--- src/main/java/net/frozenorb/potpvp/command/PStatusCommand.java
+++ src/main/java/net/frozenorb/potpvp/command/PStatusCommand.java
@@ -1,7 +1,9 @@
 package net.frozenorb.potpvp.command;
 
 import net.frozenorb.potpvp.PotPvPSI;
+import net.frozenorb.potpvp.match.Match;
 import net.frozenorb.potpvp.match.MatchHandler;
+import net.frozenorb.potpvp.match.MatchTeam;
 import net.frozenorb.potpvp.party.PartyHandler;
 import net.frozenorb.potpvp.queue.QueueHandler;
 import net.frozenorb.qlib.command.Command;
@@ -20,10 +22,40 @@ public final class PStatusCommand {
 
         sender.sendMessage(ChatColor.RED + target.getName() + ":");
         sender.sendMessage("In match: " + matchHandler.isPlayingMatch(target));
+        sender.sendMessage("In match (NC): " + noCacheIsPlayingMatch(target));
         sender.sendMessage("Spectating match: " + matchHandler.isSpectatingMatch(target));
+        sender.sendMessage("Spectating match (NC): " + noCacheIsSpectatingMatch(target));
         sender.sendMessage("In or spectating match: " + matchHandler.isPlayingOrSpectatingMatch(target));
+        sender.sendMessage("In or spectating match (NC): " + noCacheIsPlayingOrSpectatingMatch(target));
         sender.sendMessage("In queue: " + queueHandler.isQueued(target.getUniqueId()));
         sender.sendMessage("In party: " + partyHandler.hasParty(target));
+    }
+
+    private static boolean noCacheIsPlayingMatch(Player target) {
+        for (Match match : PotPvPSI.getInstance().getMatchHandler().getHostedMatches()) {
+            for (MatchTeam team : match.getTeams()) {
+                if (team.isAlive(target.getUniqueId())) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean noCacheIsSpectatingMatch(Player target) {
+        for (Match match : PotPvPSI.getInstance().getMatchHandler().getHostedMatches()) {
+            if (match.isSpectator(target.getUniqueId())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    private static boolean noCacheIsPlayingOrSpectatingMatch(Player target) {
+        return noCacheIsPlayingMatch(target) || noCacheIsSpectatingMatch(target);
     }
 
 }
