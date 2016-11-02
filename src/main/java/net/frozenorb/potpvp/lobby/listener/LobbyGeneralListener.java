716b6bee2246bc323e3002a3eab57564e0ee1acf
Colin McDonald <cmcdonald.main@gmail.com>
2016-11-01 21:01:31 -0400

    Freeze player inventories in the lobby. Closes #35

--- src/main/java/net/frozenorb/potpvp/lobby/listener/LobbyGeneralListener.java
+++ src/main/java/net/frozenorb/potpvp/lobby/listener/LobbyGeneralListener.java
@@ -5,12 +5,15 @@ import net.frozenorb.potpvp.kit.menu.editkit.EditKitMenu;
 import net.frozenorb.potpvp.match.MatchHandler;
 import net.frozenorb.qlib.menu.Menu;
 
+import org.bukkit.GameMode;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
@@ -71,6 +74,40 @@ public final class LobbyGeneralListener implements Listener {
         if (Menu.currentlyOpenedMenus.get(event.getPlayer().getName()) instanceof EditKitMenu) {
             event.getItemDrop().remove();
         } else {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onInventoryClickEvent(InventoryClickEvent event) {
+        MatchHandler matchHandler = PotPvPSI.getInstance().getMatchHandler();
+        Player clicker = (Player) event.getWhoClicked();
+        boolean creative = clicker.getGameMode() == GameMode.CREATIVE;
+
+        if (creative || matchHandler.isPlayingOrSpectatingMatch(clicker)) {
+            return;
+        }
+
+        Menu openMenu = Menu.currentlyOpenedMenus.get(clicker.getName());
+
+        if (openMenu == null || !openMenu.isNoncancellingInventory()) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onInventoryDragEvent(InventoryDragEvent event) {
+        MatchHandler matchHandler = PotPvPSI.getInstance().getMatchHandler();
+        Player clicker = (Player) event.getWhoClicked();
+        boolean creative = clicker.getGameMode() == GameMode.CREATIVE;
+
+        if (creative || matchHandler.isPlayingOrSpectatingMatch(clicker)) {
+            return;
+        }
+
+        Menu openMenu = Menu.currentlyOpenedMenus.get(clicker.getName());
+
+        if (openMenu == null || !openMenu.isNoncancellingInventory()) {
             event.setCancelled(true);
         }
     }
