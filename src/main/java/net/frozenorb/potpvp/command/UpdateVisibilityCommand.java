bcdb5b29a81b1e7fc9106913423847d53025b747
Colin McDonald <cmcdonald.main@gmail.com>
2016-11-01 22:00:06 -0400

    Add /updateVisibility command

---
+++ src/main/java/net/frozenorb/potpvp/command/UpdateVisibilityCommand.java
@@ -0,0 +1,18 @@
+package net.frozenorb.potpvp.command;
+
+import net.frozenorb.potpvp.util.InventoryUtils;
+import net.frozenorb.potpvp.util.VisibilityUtils;
+import net.frozenorb.qlib.command.Command;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+public final class UpdateVisibilityCommand {
+
+    @Command(names = {"updatevisibility", "updatevis", "upvis", "uv"}, permission = "")
+    public static void updateVisibility(Player sender) {
+        VisibilityUtils.updateVisibility(sender);
+        sender.sendMessage(ChatColor.GREEN + "Updated your visibility.");
+    }
+
+}
