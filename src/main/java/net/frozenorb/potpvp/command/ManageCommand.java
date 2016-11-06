985625c605238903d65e3d8c586c57aaf3b0e87b
Colin McDonald <cmcdonald.main@gmail.com>
2016-11-06 00:28:37 -0400

    Optimize imports

--- src/main/java/net/frozenorb/potpvp/command/ManageCommand.java
+++ src/main/java/net/frozenorb/potpvp/command/ManageCommand.java
@@ -14,7 +14,6 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
