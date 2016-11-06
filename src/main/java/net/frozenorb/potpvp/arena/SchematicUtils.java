cec4eccd1eee03e69363f6a3b19ef0d7d42fed6e
Colin McDonald <cmcdonald.main@gmail.com>
2016-11-06 12:52:49 -0500

    Use WorldEdit EditSessionFactory

--- src/main/java/net/frozenorb/potpvp/arena/SchematicUtils.java
+++ src/main/java/net/frozenorb/potpvp/arena/SchematicUtils.java
@@ -2,8 +2,11 @@ package net.frozenorb.potpvp.arena;
 
 import com.sk89q.worldedit.CuboidClipboard;
 import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.EditSessionFactory;
 import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.bukkit.BukkitWorld;
+import com.sk89q.worldedit.schematic.SchematicFormat;
 
 import org.bukkit.Bukkit;
 
@@ -12,19 +15,26 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public final class SchematicUtils {
 
-    // we use a ton of deperecated methods/classes here - due to the fact we're going to stay
-    // on 1.7.10 (and theefore same version of WorldEdit) for a while this isn't too concerning.
-    // when we have the time we should learn the WorldEdit API and update this
+    private static EditSession editSession;
+
+    public static void ensureEditSession() {
+        EditSessionFactory esFactory = WorldEdit.getInstance().getEditSessionFactory();
+        com.sk89q.worldedit.world.World weWorld = new BukkitWorld(Bukkit.getWorlds().get(0));
+
+        editSession = esFactory.getEditSession(weWorld, Integer.MAX_VALUE);
+    }
+
     public static CuboidClipboard paste(ArenaSchematic schematic, Vector pasteAt) throws Exception {
-        EditSession session = new EditSession(new BukkitWorld(Bukkit.getWorlds().get(0)), 999999999);
-        CuboidClipboard clipboard = CuboidClipboard.loadSchematic(schematic.getSchematicFile());
+        ensureEditSession();
+
+        CuboidClipboard clipboard = SchematicFormat.MCEDIT.load(schematic.getSchematicFile());
 
         // systems like the ArenaGrid assume that pastes will 'begin' directly at the Vector
         // provided. to ensure we can do this, we manually clear any offset (distance from
         // corner of schematic to player) to ensure our pastes aren't dependant on the
         // location of the player when copied
         clipboard.setOffset(new Vector(0, 0, 0));
-        clipboard.paste(session, pasteAt, false);
+        clipboard.paste(editSession, pasteAt, true);
 
         return clipboard;
     }
