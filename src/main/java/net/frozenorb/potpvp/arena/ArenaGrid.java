9b9361e95486da87d863f1db90dfb5b612b794d0
Colin McDonald <cmcdonald.main@gmail.com>
2016-12-10 22:43:22 -0500

    Lower all maps to Y=60

--- src/main/java/net/frozenorb/potpvp/arena/ArenaGrid.java
+++ src/main/java/net/frozenorb/potpvp/arena/ArenaGrid.java
@@ -32,7 +32,7 @@ public final class ArenaGrid {
     /**
      * 'Starting' point of the grid. Expands (+, +) from this point.
      */
-    public static final Vector STARTING_POINT = new Vector(1_000, 100, 1_000);
+    public static final Vector STARTING_POINT = new Vector(1_000, 60, 1_000);
 
     public static final int GRID_SPACING_X = 300;
     public static final int GRID_SPACING_Z = 300;
