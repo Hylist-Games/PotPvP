be056d11a193c25d5d894af0b664f47945958140
Colin McDonald <cmcdonald.main@gmail.com>
2016-12-16 20:19:00 -0500

    Add more detailed info to /match status

--- src/main/java/net/frozenorb/potpvp/match/command/MatchStatusCommand.java
+++ src/main/java/net/frozenorb/potpvp/match/command/MatchStatusCommand.java
@@ -14,19 +14,35 @@ import org.bukkit.entity.Player;
 public final class MatchStatusCommand {
 
     @Command(names = { "match status" }, permission = "")
-    public static void matchStatus(CommandSender sender, @Param(name = "target") Player target) {
+    public static void matchStatus(CommandSender sender, @Param(name = "target") Player target, @Param(name="scope", defaultValue = "playing") String scope) {
         MatchHandler matchHandler = PotPvPSI.getInstance().getMatchHandler();
 
-        sendMatchInfo(sender, "Cached", matchHandler.getMatchPlaying(target));
+        if (scope.equals("playing")) {
+            sendMatchInfo(sender, "Cached", matchHandler.getMatchPlaying(target));
 
-        for (Match match : matchHandler.getHostedMatches()) {
-            if (match.getTeam(target.getUniqueId()) != null) {
-                sendMatchInfo(sender, "Actual", match);
-                return;
+            for (Match match : matchHandler.getHostedMatches()) {
+                if (match.getTeam(target.getUniqueId()) != null) {
+                    sendMatchInfo(sender, "Actual", match);
+                    return;
+                }
             }
-        }
 
-        sendMatchInfo(sender, "Actual", null);
+            sendMatchInfo(sender, "Actual", null);
+        } else if (scope.equals("spectating")) {
+            sendMatchInfo(sender, "Cached (spec)", matchHandler.getMatchSpectating(target));
+
+            for (Match match : matchHandler.getHostedMatches()) {
+                if (match.isSpectator(target.getUniqueId())) {
+                    sendMatchInfo(sender, "Actual (spec)", match);
+                    return;
+                }
+            }
+
+            sendMatchInfo(sender, "Actual (spec)", null);
+        } else {
+            sender.sendMessage(ChatColor.RED + "Playing: " + matchHandler.isPlayingMatch(target));
+            sender.sendMessage(ChatColor.RED + "Spectating: " + matchHandler.isSpectatingMatch(target));
+        }
     }
 
     private static void sendMatchInfo(CommandSender sender, String title, Match match) {
