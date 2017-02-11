c854ccc5d00a13936dd1aed4d794db61f1d0d49f
Colin McDonald <cmcdonald.main@gmail.com>
2017-02-10 21:11:10 -0500

    Fix kit editor item dragging issue. Closes #17

--- src/main/java/net/frozenorb/potpvp/kit/listener/KitEditorListener.java
+++ src/main/java/net/frozenorb/potpvp/kit/listener/KitEditorListener.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 
 public final class KitEditorListener implements Listener {
 
@@ -22,6 +23,15 @@ public final class KitEditorListener implements Listener {
         if (event.getClickedInventory() != event.getView().getTopInventory()) {
             return;
         }
+
+        if (Menu.currentlyOpenedMenus.get(player.getName()) instanceof EditKitMenu) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onInventoryDrag(InventoryDragEvent event) {
+        Player player = (Player) event.getWhoClicked();
 
         if (Menu.currentlyOpenedMenus.get(player.getName()) instanceof EditKitMenu) {
             event.setCancelled(true);
--- src/main/java/net/frozenorb/potpvp/lobby/listener/LobbyGeneralListener.java
+++ src/main/java/net/frozenorb/potpvp/lobby/listener/LobbyGeneralListener.java
@@ -5,15 +5,12 @@ import net.frozenorb.potpvp.lobby.LobbyHandler;
 import net.frozenorb.potpvp.match.MatchHandler;
 import net.frozenorb.qlib.menu.Menu;
 
-import org.bukkit.GameMode;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
@@ -84,40 +81,6 @@ public final class LobbyGeneralListener implements Listener {
         if (openMenu != null && openMenu.isNoncancellingInventory()) {
             event.getItemDrop().remove();
         } else {
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler
-    public void onInventoryClickEvent(InventoryClickEvent event) {
-        MatchHandler matchHandler = PotPvPSI.getInstance().getMatchHandler();
-        Player clicker = (Player) event.getWhoClicked();
-        boolean creative = clicker.getGameMode() == GameMode.CREATIVE;
-
-        if (creative || matchHandler.isPlayingOrSpectatingMatch(clicker)) {
-            return;
-        }
-
-        Menu openMenu = Menu.currentlyOpenedMenus.get(clicker.getName());
-
-        if (openMenu == null || !openMenu.isNoncancellingInventory()) {
-            event.setCancelled(true);
-        }
-    }
-
-    @EventHandler
-    public void onInventoryDragEvent(InventoryDragEvent event) {
-        MatchHandler matchHandler = PotPvPSI.getInstance().getMatchHandler();
-        Player clicker = (Player) event.getWhoClicked();
-        boolean creative = clicker.getGameMode() == GameMode.CREATIVE;
-
-        if (creative || matchHandler.isPlayingOrSpectatingMatch(clicker)) {
-            return;
-        }
-
-        Menu openMenu = Menu.currentlyOpenedMenus.get(clicker.getName());
-
-        if (openMenu == null || !openMenu.isNoncancellingInventory()) {
             event.setCancelled(true);
         }
     }
