5b79c1a928157583693cac773095152862feac55
Colin McDonald <cmcdonald.main@gmail.com>
2017-02-13 18:28:59 -0500

    Run setting load/unload code at the proper priority

--- src/main/java/net/frozenorb/potpvp/setting/listener/SettingLoadListener.java
+++ src/main/java/net/frozenorb/potpvp/setting/listener/SettingLoadListener.java
@@ -3,18 +3,19 @@ package net.frozenorb.potpvp.setting.listener;
 import net.frozenorb.potpvp.PotPvPSI;
 
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public final class SettingLoadListener implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST) // LOWEST runs first
     public void onAsyncPlayerPreLogin(AsyncPlayerPreLoginEvent event) {
         PotPvPSI.getInstance().getSettingHandler().loadSettings(event.getUniqueId());
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR) // MONITOR runs last
     public void onPlayerQuit(PlayerQuitEvent event) {
         PotPvPSI.getInstance().getSettingHandler().unloadSettings(event.getPlayer().getUniqueId());
     }
