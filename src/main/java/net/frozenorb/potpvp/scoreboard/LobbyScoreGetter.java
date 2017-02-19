5ffa90364db9889c3e2d5f5ed961451f21781900
Colin McDonald <cmcdonald.main@gmail.com>
2017-02-19 10:38:44 -0500

    Change scoreboard layout, prevent negative elo search range

--- src/main/java/net/frozenorb/potpvp/scoreboard/LobbyScoreGetter.java
+++ src/main/java/net/frozenorb/potpvp/scoreboard/LobbyScoreGetter.java
@@ -55,15 +55,14 @@ final class LobbyScoreGetter implements BiConsumer<Player, List<String>> {
             MatchQueue queue = entry.getQueue();
 
             scores.add("&b&7&m--------------------");
-            scores.add("&7Queued for");
             scores.add(queue.getKitType().getDisplayColor() + (queue.isRanked() ? "Ranked" : "Unranked") + " " + queue.getKitType().getName());
-            scores.add("&eTime: *&7" + waitTimeFormatted);
+            scores.add("&eTime: *&f" + waitTimeFormatted);
 
             if (queue.isRanked()) {
                 int elo = eloHandler.getElo(entry.getMembers(), queue.getKitType());
                 int window = entry.getWaitSeconds() * QueueHandler.RANKED_WINDOW_GROWTH_PER_SECOND;
 
-                scores.add("&eSearch range: *&7" + (elo - window) + " - " + (elo + window));
+                scores.add("&eSearch range: *&f" + Math.max(0, elo - window) + " - " + (elo + window));
             }
         }
     }
