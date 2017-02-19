72c0094cb75415f117d5ce8e60e0677993d4f289
Colin McDonald <cmcdonald.main@gmail.com>
2017-02-19 10:40:03 -0500

    Fix ItemListener memory leak

--- src/main/java/net/frozenorb/potpvp/util/ItemListener.java
+++ src/main/java/net/frozenorb/potpvp/util/ItemListener.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
@@ -55,6 +56,11 @@ public abstract class ItemListener implements Listener {
                 return;
             }
         }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        canUseButton.remove(event.getPlayer().getUniqueId());
     }
 
 }
