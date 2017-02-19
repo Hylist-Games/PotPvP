90dafda415aba4c548e6f52a12ccd504ba6a0095
Colin McDonald <cmcdonald.main@gmail.com>
2017-02-19 12:37:25 -0500

    Cleanup admin commands

--- src/main/java/net/frozenorb/potpvp/command/MatchListCommand.java
+++ src/main/java/net/frozenorb/potpvp/command/MatchListCommand.java
@@ -1,4 +1,4 @@
-package net.frozenorb.potpvp.match.command;
+package net.frozenorb.potpvp.command;
 
 import net.frozenorb.potpvp.PotPvPSI;
 import net.frozenorb.potpvp.match.Match;
@@ -8,10 +8,10 @@ import net.frozenorb.qlib.command.Command;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
-public final class ListMatchesCommand {
+public final class MatchListCommand {
 
-    @Command(names = { "listMatches" }, permission = "op")
-    public static void listMatches(Player sender) {
+    @Command(names = { "match list" }, permission = "op")
+    public static void matchList(Player sender) {
         MatchHandler matchHandler = PotPvPSI.getInstance().getMatchHandler();
 
         for (Match match : matchHandler.getHostedMatches()) {
