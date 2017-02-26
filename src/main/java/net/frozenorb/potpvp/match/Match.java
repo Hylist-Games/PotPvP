272697a80ac515261726f1f433ede7a689db64ca
Colin McDonald <cmcdonald.main@gmail.com>
2017-02-25 19:01:03 -0500

    Fix match description

--- src/main/java/net/frozenorb/potpvp/match/Match.java
+++ src/main/java/net/frozenorb/potpvp/match/Match.java
@@ -396,7 +396,7 @@ public final class Match {
                 numTotalPlayers += team.getAliveMembers().size();
             }
 
-            players = numTotalPlayers + " player FFA";
+            players = numTotalPlayers + " player fight";
         }
 
         String rankedStr = ranked ? "Ranked" : "Unranked";
