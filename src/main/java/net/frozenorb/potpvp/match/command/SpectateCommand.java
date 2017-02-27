e684bfd81569b3b3f1a308f462e6e2ce8094da6f
Colin McDonald <cmcdonald.main@gmail.com>
2017-02-27 18:10:39 -0500

    Require all players in a 1v1 have spectator mode enable

--- src/main/java/net/frozenorb/potpvp/match/command/SpectateCommand.java
+++ src/main/java/net/frozenorb/potpvp/match/command/SpectateCommand.java
@@ -3,14 +3,18 @@ package net.frozenorb.potpvp.match.command;
 import net.frozenorb.potpvp.PotPvPSI;
 import net.frozenorb.potpvp.match.Match;
 import net.frozenorb.potpvp.match.MatchHandler;
+import net.frozenorb.potpvp.match.MatchTeam;
 import net.frozenorb.potpvp.setting.Setting;
 import net.frozenorb.potpvp.setting.SettingHandler;
 import net.frozenorb.potpvp.validation.PotPvPValidation;
 import net.frozenorb.qlib.command.Command;
 import net.frozenorb.qlib.command.Param;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+
+import java.util.UUID;
 
 public final class SpectateCommand {
 
@@ -31,12 +35,31 @@ public final class SpectateCommand {
             return;
         }
 
-        if (!settingHandler.getSetting(target, Setting.ALLOW_SPECTATORS)) {
+        // only check the seting if the target is actually playing in the match
+        if (targetMatch.getTeam(target.getUniqueId()) != null && !settingHandler.getSetting(target, Setting.ALLOW_SPECTATORS)) {
             if (sender.isOp()) {
                 sender.sendMessage(ChatColor.RED + "Bypassing " + target.getName() + "'s no spectators preference...");
             } else {
                 sender.sendMessage(ChatColor.RED + target.getName() + " doesn't allow spectators at the moment.");
                 return;
+            }
+        }
+
+        if (!sender.isOp() && targetMatch.getTeams().size() == 2) {
+            MatchTeam teamA = targetMatch.getTeams().get(0);
+            MatchTeam teamB = targetMatch.getTeams().get(1);
+
+            if (teamA.getAllMembers().size() == 1 && teamB.getAllMembers().size() == 1) {
+                UUID teamAPlayer = teamA.getAllMembers().iterator().next();
+                UUID teamBPlayer = teamB.getAllMembers().iterator().next();
+
+                if (
+                    !settingHandler.getSetting(Bukkit.getPlayer(teamAPlayer), Setting.ALLOW_SPECTATORS) ||
+                    !settingHandler.getSetting(Bukkit.getPlayer(teamBPlayer), Setting.ALLOW_SPECTATORS)
+                ) {
+                    sender.sendMessage(ChatColor.RED + "Not all players in that 1v1 have spectators enabled.");
+                    return;
+                }
             }
         }
 
