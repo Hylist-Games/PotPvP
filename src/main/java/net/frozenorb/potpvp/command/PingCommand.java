71a16913b6aee5fab5d65dec581e63be00a0545f
Francisco <francisco.gsaldanha@gmail.com>
2017-03-01 00:50:59 -0300

    Add /ping

---
+++ src/main/java/net/frozenorb/potpvp/command/PingCommand.java
@@ -0,0 +1,18 @@
+package net.frozenorb.potpvp.command;
+
+import net.frozenorb.qlib.command.Command;
+import net.frozenorb.qlib.command.Param;
+import net.frozenorb.qlib.util.PlayerUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+public class PingCommand {
+
+    @Command(names = "ping", permission = "")
+    public static void ping(Player sender, @Param(name = "target", defaultValue = "self") Player target) {
+        int ping = PlayerUtils.getPing(target);
+
+        sender.sendMessage(target.getDisplayName() + ChatColor.YELLOW + "'s Ping: " + ChatColor.RED + ping);
+    }
+
+}
