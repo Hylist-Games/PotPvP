1d040632a48a47b5f53dd2f6ac0783146ba8c506
Colin McDonald <cmcdonald.main@gmail.com>
2017-03-05 17:46:40 -0500

    Prevent party chat spam

--- src/main/java/net/frozenorb/potpvp/party/listener/PartyChatListener.java
+++ src/main/java/net/frozenorb/potpvp/party/listener/PartyChatListener.java
@@ -9,29 +9,41 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
 public final class PartyChatListener implements Listener {
 
-    @EventHandler
+    private final Map<UUID, Long> canUsePartyChat = new ConcurrentHashMap<>();
+
+    @EventHandler(ignoreCancelled = true)
     public void onAsyncPlayerChat(AsyncPlayerChatEvent event) {
         if (!event.getMessage().startsWith("@")) {
             return;
         }
 
+        event.setCancelled(true);
+
         Player player = event.getPlayer();
-        String message = event.getMessage().substring(1);
+        String message = event.getMessage().substring(1).trim();
         Party party = PotPvPSI.getInstance().getPartyHandler().getParty(player);
 
         if (party == null) {
             player.sendMessage(ChatColor.RED + "You aren't in a party!");
-            event.setCancelled(true);
+            return;
+        }
+
+        if (canUsePartyChat.getOrDefault(player.getUniqueId(), 0L) > System.currentTimeMillis()) {
+            player.sendMessage(ChatColor.RED + "Wait a bit before sending another message.");
             return;
         }
 
         ChatColor prefixColor = party.isLeader(player.getUniqueId()) ? ChatColor.AQUA : ChatColor.LIGHT_PURPLE;
         party.message(prefixColor.toString() + ChatColor.BOLD + "[P] " + player.getName() + ": " + ChatColor.LIGHT_PURPLE + message);
 
+        canUsePartyChat.put(player.getUniqueId(), System.currentTimeMillis() + 2_000);
         PotPvPSI.getInstance().getLogger().info("[Party Chat] " + player.getName() + ": " + message);
-        event.setCancelled(true);
     }
 
 }
